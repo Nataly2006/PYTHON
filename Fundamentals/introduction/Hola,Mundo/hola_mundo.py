8c96007cf3edc531cd17f8e11a75561a8dd20926
nataly <natalyepul98@gmail.com>
2023-04-25 14:07:36 -0400

    python

--- Fundamentals/introduction/Hola,Mundo/hola_mundo.py
+++ Fundamentals/introduction/Hola,Mundo/hola_mundo.py
@@ -3,15 +3,16 @@ print( "Hola, Mundo")
 
 # 2. imprime "Hola, Nataly" con el nombre en una variable
 name = "Nataly"
-print( "Hola, " + name )	# con una coma y un +
+print( "Hola, " + name )  # con una coma y un +
 
 # 3. imprimir "Hola 42!" con el número en una variable
 name = 42
-print( tu código aquí )	# con una coma
-print( tu código aquí )	# con una +	-- este debería arrojar un error!
+print("Hola " + str(42) + "!" )	# con una coma
+print("Hola " + str(42) + --  "!" )  # con una +	-- este debería arrojar un error!
+
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( tu código aquí ) # con .format()
-print( tu código aquí ) # con una cadena f
+print("Amo comer y {} {} ".format( fave_food1, fave_food2))# con .format()
+print(f"Amo comer y {fave_food1} {fave_food2}") # con una cadena f
 
